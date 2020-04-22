eaefd18e114a1b4882393694a8e86d5decf6023a
Alfpercar <alfonso.perez@upf.edu>
2020-04-22 15:46:46 +0200

    Alfonso first changes in other scripts

--- getMagRes.m
+++ getMagRes.m
@@ -1,15 +1,20 @@
-function output = getMagRes(x, nAF)
-h = load("PB_filters.mat");
+function output = getMagRes(x, fcs, fs)
 
-for i = 1:nAF
-    band = conv(x, h.imp{i});
-    rms = sqrt(1/size(band, 1)*sum(band.^2));
+for iband = 2:length(fcs)-1
+    %butterworth fiter at fc(iband)
+    fc = [fcs(iband-1), fcs(iband+1)] ./ (fs/2);  
+    fc(2) = min(fc(2),0.9999);
+    [b,a] = butter(2,fc,'bandpass'); 
+    %freqz(b,a)
+    filtered = filter(b,a,x);
+
+    rms = sqrt(1/size(filtered, 1)*sum(filtered.^2));
     if(rms ~= 0)
-        output(i) = 10*log10(rms);
+        output(iband-1) = 10*log10(rms);
     else
         %output(i) = -100000;
-        output(i) = -Inf;
-end
+        output(iband-1) = -Inf;
+    end
     
 end
 % 
--- selectMasking.m
+++ selectMasking.m
@@ -1,19 +1,21 @@
 function output = selectMasking(M, nF)
 
-for i = 1:size(M, 2)
-    masking(i) = max(M(1,:,i));
-end
-%Take nF higher values and save the index value (bin number)
-aux = zeros(nF, 2);
-for i = 1:nF
-    [aux(i, 2), aux(i, 1)] = max(masking);
-    masking(aux(i, 1)) = 0;
-end
+output = max(squeeze(M))
 
-%Sort by bin number
-aux = sortrows(aux);
-
-%Take only the masking value
-output = aux(:, 2)';
+% for i = 1:size(M, 2)
+%     masking(i) = max(M(1,:,i));
+% end
+% %Take nF higher values and save the index value (bin number)
+% aux = zeros(nF, 2);
+% for i = 1:nF
+%     [aux(i, 2), aux(i, 1)] = max(masking);
+%     masking(aux(i, 1)) = 0;
+% end
+% 
+% %Sort by bin number
+% aux = sortrows(aux);
+% 
+% %Take only the masking value
+% output = aux(:, 2)';
 
 end
